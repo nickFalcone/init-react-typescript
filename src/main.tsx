44eb88a60dfd900d5d4047165440f712dc838e45
nickFalcone <nfalcone@u.rochester.edu>
2022-12-21 11:11:10 -0500

    no strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import './index.css';
 
 import { worker } from 'mocks/browser';
-import React, { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
@@ -11,9 +11,7 @@ if (process.env.NODE_ENV === 'test') {
   worker.start();
 }
 
-const container = document.getElementById('container') as Element;
-createRoot(container).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const container = document.getElementById('container');
+if (container) {
+  createRoot(container).render(<App />);
+}
